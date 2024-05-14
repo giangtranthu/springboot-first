bd6b6174f8c8d3f77dd7f36116c5ae883b4a372c
thugiang0 <thugiangtran00@gmail.com>
2024-05-14 09:47:35 +0700

    edit controller

--- src/main/java/com/springboot/springbootfirst/WelcomeController.java
+++ src/main/java/com/springboot/springbootfirst/WelcomeController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class WelcomeController {
 	@GetMapping("/hello")
 	public String welcome() {
-		return "Welcome to spring boot";
+		return "Hello world";
 	}
 
 
